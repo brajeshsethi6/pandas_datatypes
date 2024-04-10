d04ae73dc0ed940d1ab1c72f3af6641fbf16f956
Brajesh Kumar Sethi <68315434+brajeshsethi6@users.noreply.github.com>
2024-04-11 04:42:28 +0530

    feat:added readme as long description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="pandas_datatypes",
     version="0.1.0",
     author="Brajesh",
-    description="A library for geting correct datatype from pandas dataFrame",
+    description="A library for getting correct datatype from pandas DataFrame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(include=["pandas_datatypes", "pandas_datatypes.*"]),
     install_requires=[
         "pandas",
